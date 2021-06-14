00d38f666ef9bb23d76b39224f20b74fe46725a3
AyushGaur-1704 <85785196+AyushGaur-1704@users.noreply.github.com>
2021-06-14 17:35:46 +0530

    Add files via upload

---
+++ Java/AyushGaur_1922014/ques.java
@@ -0,0 +1,9 @@
+class Program
+{
+	public static void main(String[] args)
+	{
+		String Name="Ayush";
+		System.out.println("Before Using ToUpperCase:"+Name);
+		System.out.println("After Using ToUpperCase:"+Name.toUpperCase());
+	}
+}
---
+++ Java/AyushGaur_1922014/ques2.java
@@ -0,0 +1,7 @@
+class Program
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Name:Ayush Gaur"+"\t RollNo:1922014");
+	}
+}
